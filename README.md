6e52a04f8320627d8de51fc0f0524fb06c349165
Jalal <Jalalx@msn.com>
2020-12-16 15:51:53 +0500

    Final Project

---
+++ README.md
@@ -0,0 +1,4 @@
+# React Generate Table Task 
+
+This a simple react task (for ContextAPI use) for generating multiplication tables in the range provided by the user.
+
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import GenerateTable from './GenerateTable';
+ 
+function App() {
+  return (
+    <React.Fragment>
+      <GenerateTable />
+    </React.Fragment>
+  );
+}
+
+export default App;
